893bf9443613cc1546e35aa98fc8a3f2bfb5b78e
prateek tiwari <tiwariprateek1976@gmail.com>
2025-01-12 15:02:44 +0530

    cors5

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ dotenv.config();
 
 app.use(cors({
     origin: '*',
-    credentials: true,
+   
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+   
 }));
 
 
